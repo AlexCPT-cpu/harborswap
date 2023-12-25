dd011d1274072d5f70797d78125d7ca73dc083e6
AlexCPT-cpu <alexodey79@gmail.com>
2023-12-25 11:37:14 +0100

    roll changes

--- components/Modals/TokenModal.tsx
+++ components/Modals/TokenModal.tsx
@@ -14,10 +14,12 @@ import axios from "axios";
 
 export default function TokenModal() {
   const tokens = useMemo(() => {
-    const saved = localStorage.getItem("user-saved-tokens");
-    const parsed = JSON.parse(saved!);
-    const all = [...tokenList, ...parsed];
-    return all;
+    if (typeof window !== "undefined") {
+      const saved = localStorage.getItem("user-saved-tokens");
+      const parsed = JSON.parse(saved!);
+      const all = [...tokenList, ...parsed];
+      return all;
+    }
   }, []);
   const { modalState, toogleModal, modalIndex, toogleIndex } = useModal();
   const { handleIn, handleOut, coinIn, coinOut } = useCoin();
@@ -48,7 +50,7 @@ export default function TokenModal() {
 
   const filtered = useMemo(
     () =>
-      tokens.filter((token) => {
+      tokens?.filter((token) => {
         if (isAddress) {
           if (token.address === input) {
             return token;
@@ -64,32 +66,39 @@ export default function TokenModal() {
   useEffect(() => {
     const findToken = async () => {
       try {
-        const { data } = await axios.post("/api/token", { contract: input });
-        const formated = {
-          name: data.name,
-          symbol: data.symbol,
-          logoURI: data.logo ?? "",
-          address: input,
-          decimals: data.decimals,
-        };
-        setSearched([formated]);
-        const saved = localStorage.getItem("user-saved-tokens");
-        if (saved) {
-          const parsed = JSON.parse(saved);
-          const exists = parsed.some((obj: any) => obj.address === input);
+        setTimeout(async () => {
+          const { data } = await axios.post("/api/token", { contract: input });
+          const formated = {
+            name: data.name,
+            symbol: data.symbol,
+            logoURI: data.logo ?? "",
+            address: input,
+            decimals: data.decimals,
+          };
+          setSearched([formated]);
+          const saved = localStorage.getItem("user-saved-tokens");
+          if (saved) {
+            const parsed = JSON.parse(saved);
+            const exists = parsed.some((obj: any) => obj.address === input);
 
-          if (!exists) {
-            const final = [...parsed, formated];
+            if (!exists) {
+              const final = [...parsed, formated];
 
-            localStorage.setItem("user-saved-tokens", JSON.stringify(final));
+              localStorage.setItem("user-saved-tokens", JSON.stringify(final));
+            }
+          } else {
+            localStorage.setItem(
+              "user-saved-tokens",
+              JSON.stringify([formated])
+            );
           }
-        } else {
-          localStorage.setItem("user-saved-tokens", JSON.stringify([formated]));
-        }
+        }, 1000);
       } catch (error) {}
     };
-    if (isAddress && filtered.length == 0) {
-      findToken();
+    if (typeof window !== "undefined") {
+      if (isAddress && filtered?.length == 0) {
+        findToken();
+      }
     }
   }, [filtered, input, isAddress]);
   return (
@@ -171,7 +180,7 @@ export default function TokenModal() {
                   </div>
                 ) : (
                   <>
-                    {filtered.length > 0 ? (
+                    {filtered?.length! > 0 ? (
                       <div className="mt-4 flex w-full flex-col pb-15 h-[420px] border-b-black/50 dark:border-b-white/1 overflow-auto space-y-2 scroll-smooth transition-all">
                         {filtered?.map((token, index) => {
                           const disabled =
--- pages/api/token.ts
+++ pages/api/token.ts
@@ -1,3 +1,4 @@
+import { delay } from "@/helpers/delay";
 import { Alchemy, Network } from "alchemy-sdk";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -8,7 +9,8 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const { contract } = req.body;
-
+    const randomNumber = Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000;
+   await delay(randomNumber)
     const config = {
       apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY,
       network: Network.ETH_MAINNET,
